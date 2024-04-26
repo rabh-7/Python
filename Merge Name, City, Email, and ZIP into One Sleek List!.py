b3b03dacd05ff20647cd4aa85fa07a0d4930c50b
SAURABH SINGH <153974307+rabh-7@users.noreply.github.com>
2024-04-26 22:39:04 +0530

    Delete Merge Name, City, Email, and ZIP into One Sleek List!.py

--- Merge Name, City, Email, and ZIP into One Sleek List!.py
+++
@@ -1,7 +0,0 @@
-#list of three individual info i.e. name, city, mail and zip as single.
-
-name=['Abhi','Max','Sam','Sim']
-city=['Delhi','sydney','Dubai','Hazaribagh']
-mail=['abhi90@gmail.com','max191@gmail.com','sam21@gmail.com','sim@gmail.com']
-data=list(zip(name,city,mail))
-print(data)
